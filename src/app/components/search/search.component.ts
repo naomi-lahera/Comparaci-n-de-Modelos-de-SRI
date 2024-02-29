e980c066f0d0f48c7f22d5a1389626209aa3d3eb
Naomi Lahera <lahera0103@gmail.com>
2024-02-29 01:50:50 -0500

    ok

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -29,10 +29,11 @@ export class SearchComponent implements OnInit {
           labels: ['Booleano', 'Booleano Extendido'],
           datasets: [
               {
-                  data: [540, 325],
-                  backgroundColor: ['rgba(255, 159, 64, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(153, 102, 255, 0.2)'],
-                  borderColor: ['rgb(255, 159, 64)', 'rgb(75, 192, 192)', 'rgb(54, 162, 235)', 'rgb(153, 102, 255)'],
-                  borderWidth: 1
+                label: 'Sales',
+                data: [0.540, 0.325],
+                backgroundColor: ['rgba(255, 159, 64, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(153, 102, 255, 0.2)'],
+                borderColor: ['rgb(255, 159, 64)', 'rgb(75, 192, 192)', 'rgb(54, 162, 235)', 'rgb(153, 102, 255)'],
+                borderWidth: 1
               }
           ]
       } 
